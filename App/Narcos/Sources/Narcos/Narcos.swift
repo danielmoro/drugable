a5171f5c59a0220604d9b6ccca140b066a9c73f8
Mirjana Milic <mirjana86@gmail.com>
2021-01-18 11:31:35 +0100

    edit reminder test

--- App/Narcos/Sources/Narcos/Narcos.swift
+++ App/Narcos/Sources/Narcos/Narcos.swift
@@ -1,21 +1,24 @@
 
 protocol Router {
     func navigateToHome()
-    func navigateToNewReminder(with completion: ((Reminder?) -> Void))
+    func navigateToNewReminder(with completion: @escaping ((Reminder?) -> Void))
+    func navigateToEditReminder(reminder: Reminder, with completion: @escaping ((Reminder) -> Void))
+
 }
 
-struct Reminder {
-    
+struct Reminder: Equatable {
+    var name: String
 }
 
 class Narcos {
     
     private var router: Router
     
-    var reminders: [Reminder] = []
+    var reminders: [Reminder]
     
-    init(router: Router) {
+    init(router: Router, reminders: [Reminder] = []) {
         self.router = router
+        self.reminders = reminders
     }
     
     func start() {
@@ -23,12 +26,19 @@ class Narcos {
     }
     
     func createReminder() {
-        router.navigateToNewReminder { (reminder) in
+        router.navigateToNewReminder {[weak self] (reminder) in
             if let reminder = reminder {
-                reminders.append(reminder)
+                self?.reminders.append(reminder)
             }
             
-            router.navigateToHome()
+            self?.router.navigateToHome()
+        }
+    }
+    
+    func editReminder(at index: Int) {
+        router.navigateToEditReminder(reminder: reminders[index]) {[weak self] (reminder) in
+            self?.reminders.remove(at: index)
+            self?.reminders.insert(reminder, at: index)
         }
     }
 }
--- App/Narcos/Tests/NarcosTests/NarcosTests.swift
+++ App/Narcos/Tests/NarcosTests/NarcosTests.swift
@@ -16,7 +16,7 @@ final class NarcosTests: XCTestCase {
     
     func test_createReminderAndCommits_remindersCountIncrements() throws {
         let (sut, router) = makeSUT()
-        router.newReminder = Reminder()
+        router.newReminder = makeReminder()
         
         sut.start()
         sut.createReminder()
@@ -36,17 +36,62 @@ final class NarcosTests: XCTestCase {
         XCTAssertEqual(router.routes, ["home", "new reminder", "home"])
     }
     
-    private func makeSUT() -> (Narcos, RouterSpy) {
-        let router = RouterSpy()
-        return (Narcos(router: router), router)
+    func test_editExistingReminder_reminderChanged() {
+        let reminder1 = makeReminder()
+        let reminder2 = makeReminder()
+        let reminders = [reminder1, reminder2]
+        let (sut, router) = makeSUT(reminders: reminders)
+        router.editReminderCompletion = { reminder in
+            if reminder == reminder1 {
+                var reminderNew = reminder
+                reminderNew.name = "updatedName"
+                return reminderNew
+            } else {
+                var reminderNew = reminder
+                reminderNew.name = "updatedName1"
+                return reminderNew
+            }
+        }
+        sut.editReminder(at: 0)
+        XCTAssertEqual(sut.reminders[0].name, "updatedName")
+        
+        sut.editReminder(at: 1)
+        XCTAssertEqual(sut.reminders[1].name, "updatedName1")
+        
     }
     
+    func test_startWithExistingReminders_validReminerCount() {
+        let reminders = [makeReminder(), makeReminder()]
+        let (sut, router) = makeSUT(reminders: reminders)
+        
+        XCTAssertEqual(sut.reminders.count, 2)
+    }
+
+    //MARK: -
+    
+    private func makeReminder() -> Reminder {
+        
+        return Reminder(name: "")
+    }
+    private func makeSUT(reminders: [Reminder] = []) -> (Narcos, RouterSpy) {
+        let router = RouterSpy()
+        return (Narcos(router: router, reminders: reminders), router)
+    }
+
+    
     private class RouterSpy: Router {
+        
         var newReminder : Reminder?
+        var editReminderCompletion: ((Reminder) -> Reminder)?
         
         func navigateToNewReminder(with completion: ((Reminder?) -> Void)) {
             routes.append("new reminder")
             completion(newReminder)
+        }
+        
+        func navigateToEditReminder(reminder: Reminder, with completion: @escaping ((Reminder) -> Void)) {
+            completion(editReminderCompletion?(reminder) ?? reminder)
+            
         }
         
         var routesCount: Int {
